054217e4c39791f9f44f4be682491adc689065ec
Massimo Ronca <massimo@mikamai.com>
2016-12-06 17:56:34 +0100

    fix provider call

--- lib/geocoder.ex
+++ lib/geocoder.ex
@@ -40,11 +40,11 @@ defmodule Geocoder do
 
   def call(q, opts \\ [])
   def call(q, opts) when is_binary(q), do: Worker.geocode(opts ++ [address: q])
-  def call(q = {lat,lon}, opts), do: Worker.reverse_geocode(opts ++ [lat: lat, lon: lon, latlng: q])
+  def call(q, opts), do: Worker.reverse_geocode(opts ++ [latlng: q])
   def call(%{lat: lat, lon: lon}, opts), do: call({lat, lon}, opts)
 
   def call_list(q, opts \\ [])
   def call_list(q, opts) when is_binary(q), do: Worker.geocode_list(opts ++ [address: q])
-  def call_list(q = {_,_}, opts), do: Worker.reverse_geocode_list(opts ++ [latlng: q])
+  def call_list(q, opts), do: Worker.reverse_geocode_list(opts ++ [latlng: q])
   def call_list(%{lat: lat, lon: lon}, opts), do: call_list({lat, lon}, opts)
 end
